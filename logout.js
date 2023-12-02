f9701f93c2c3adc925d7e9ce6c3549eb65fd5007
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2023-12-02 22:39:54 +0700

    add logout js function

---
+++ logout.js
@@ -0,0 +1,22 @@
+const logout = () => {
+    // Menampilkan Sweet Alert konfirmasi
+    Swal.fire({
+      icon: "question",
+      title: "Confirmation",
+      text: "Are you sure you want to log out?",
+      showCancelButton: true,
+      confirmButtonText: "Yes",
+      cancelButtonText: "Cancel",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("previousPageURL");
+        // Menghapus token dari cookie
+        document.cookie =
+          "Authorization= ; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        // Redirect ke halaman sign-in.html
+        window.location.href = "../signin.html";
+      }
+    });
+  };
+  
+  window.logout = logout;
